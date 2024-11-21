ed12f63980911aea50df6826840e5ec97303e6e7
riskalaluna <riskalaluna@gmail.com>
2024-11-21 23:45:22 +0700

    membuat Row didalam Column

--- app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
@@ -2,14 +2,19 @@ package com.example.praktikum7_176.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.praktikum7_176.R
 import com.example.praktikum7_176.model.Mahasiswa
 
@@ -33,6 +38,14 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
 
     }
 
