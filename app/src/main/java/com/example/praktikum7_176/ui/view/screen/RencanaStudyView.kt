c1033e7b28b62bbbef36957e7966ed93476cfe9e
riskalaluna <riskalaluna@gmail.com>
2024-11-21 23:41:15 +0700

    menuliskan variable pilihanKelas dan listData

--- app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum7_176.ui.view.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +16,15 @@ fun RencanaStudyView(
 ) {
     var chosenDropdown by remember  {
         mutableStateOf("")
+    }
+    var checked by remember {
+        mutableStateOf(false) }
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column {
+
     }
 
 }
