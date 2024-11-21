76fea108184cec49ee6a9f5cf307abbe7d30ccea
riskalaluna <riskalaluna@gmail.com>
2024-11-22 00:45:46 +0700

    menyelesaikan codingan di RencanaStudyViewModel

--- app/src/main/java/com/example/praktikum7_176/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.praktikum7_176.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStudiUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan)}
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update {stateKelas -> stateKelas.copy(kelas = kelasPilihan)}
+    }
+
+    fun SaveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status -> status.copy(
+            namaMK = ls[0],
+            kelas = ls[1]
+        )}
+    }
 }
