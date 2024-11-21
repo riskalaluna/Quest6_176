45b6d5aa30c718c2c9241379c1fb49da4b22177a
riskalaluna <riskalaluna@gmail.com>
2024-11-21 23:55:31 +0700

    mengatur text untuk nama

--- app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,9 +19,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktikum7_176.R
 import com.example.praktikum7_176.model.Mahasiswa
 
@@ -59,6 +63,12 @@ fun RencanaStudyView(
             )
             Spacer(modifier = Modifier.padding(start = 16.dp))
             Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
 
             }
 
