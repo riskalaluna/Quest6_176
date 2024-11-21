a86e0c133491138c0c8ba099c8eeea323bda84b1
riskalaluna <riskalaluna@gmail.com>
2024-11-21 14:38:53 +0700

    menuliskan code di file MataKuliah

--- app/src/main/java/com/example/praktikum7_176/data/MataKuliah.kt
+++ app/src/main/java/com/example/praktikum7_176/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.praktikum7_176.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
 }
