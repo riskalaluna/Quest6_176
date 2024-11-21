c91c5ae76b0d8342c982db5f8667805a901d5bc7
riskalaluna <riskalaluna@gmail.com>
2024-11-21 23:49:38 +0700

    menambahkan image

--- app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/screen/RencanaStudyView.kt
@@ -1,11 +1,14 @@
 package com.example.praktikum7_176.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,7 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.praktikum7_176.R
 import com.example.praktikum7_176.model.Mahasiswa
@@ -44,6 +49,13 @@ fun RencanaStudyView(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ){
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
 
         }
 
