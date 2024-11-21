1ed57bf1231a17c56b09b115126c96b6205292f4
riskalaluna <riskalaluna@gmail.com>
2024-11-21 23:12:33 +0700

    mengatur keyboard menjadi number

--- app/src/main/java/com/example/praktikum7_176/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/view/screen/MahasiswaFormView.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Info
@@ -32,6 +33,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -119,12 +122,11 @@ fun MahasiswaFormView (
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier
-                    .padding(4.dp))
+                    .padding(8.dp))
 
                 OutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    shape = RoundedCornerShape(50.dp),
                     value = nim,
                     onValueChange = {nim = it},
                     label = { Text(text = "Nomor Induk Mahasiswa")},
@@ -132,7 +134,11 @@ fun MahasiswaFormView (
                         Icon(imageVector = Icons.Filled.Info,
                             contentDescription = " "
                         )
-                    }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    ),
                 )
                 Spacer(modifier = Modifier
                     .padding(4.dp))
