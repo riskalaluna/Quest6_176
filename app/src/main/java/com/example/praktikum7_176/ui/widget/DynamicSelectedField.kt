8b648a768b40b50c686d9154eb494fc766dbc962
riskalaluna <riskalaluna@gmail.com>
2024-11-21 21:59:38 +0700

    membuat komponen dropdown dinamis di Jetpack Compose

--- app/src/main/java/com/example/praktikum7_176/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikum7_176/ui/widget/DynamicSelectedField.kt
@@ -1,1 +1,20 @@
 package com.example.praktikum7_176.ui.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false)}
+}
